f218323ceca2bc674e96000ab3aff92025568ca6
Maxim Yuriev <yrev2011@gmail.com>
2024-10-03 21:26:41 +0700

    tryRedisConnect

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ bot = Bot(token=BOT_TOKEN)
 dispatcher = Dispatcher()
 
 async def get_dollar_rate():
-    #async with Redis(host=REDIS_HOST, port=REDIS_PORT) as redis_client:
-    #    value = await redis_client.get(KEY_NAME)
-    #    if value is not None:
-    #        return f"{float(value)} рублей"
+    async with Redis(host=REDIS_HOST, port=REDIS_PORT) as redis_client:
+        value = await redis_client.get(KEY_NAME)
+        if value is not None:
+            return f"{float(value)} рублей"
 
-    async with aiohttp.ClientSession() as session:
-        response = await session.get('https://www.cbr-xml-daily.ru/latest.js')
-        if response.status == 200:
-            text = await response.text()
-            data = json.loads(text)
-            dollar_rate = 1/data['rates']['USD']
-            #await redis_client.set(KEY_NAME,dollar_rate,60)
-            return f"{dollar_rate} рублей"
-        return None
+        async with aiohttp.ClientSession() as session:
+            response = await session.get('https://www.cbr-xml-daily.ru/latest.js')
+            if response.status == 200:
+                text = await response.text()
+                data = json.loads(text)
+                dollar_rate = 1/data['rates']['USD']
+                await redis_client.set(KEY_NAME,dollar_rate,60)
+                return f"{dollar_rate} рублей"
+            return None
 
 
 @dispatcher.message(CommandStart())
